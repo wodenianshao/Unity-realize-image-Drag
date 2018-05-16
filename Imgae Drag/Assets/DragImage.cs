6963d0096201debddff32037b133c8d444018e6c
hxd <wodenianshao@163.com>
2018-05-16 15:45:20 +0800

    图片拖拽和实体的应用
    图片拖拽和实体的应用

---
+++ Imgae Drag/Assets/DragImage.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragImage : MonoBehaviour,IBeginDragHandler,IEndDragHandler,IDragHandler
+{
+    public RectTransform rectTransform;
+    // Use this for initialization
+    void Start () {
+        rectTransform = this.gameObject.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //实现接口，开始拖拽
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        print("开始拖拽");
+    }
+
+    //实现接口，拖拽时执行的方法
+    public void OnDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("拖拽中x:{0}  y:{1}"+ eventData.position.x+" "+eventData.position.y);
+        rectTransform.position = eventData.position;
+    }
+
+    //实现接口，结束拖拽
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        print("结束拖拽");
+    }
+
+ 
+}
